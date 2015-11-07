6acc7bbdc089f2cf11b1629a069ca054febb08c5
Longyang Zhang <zhanglongyang.cn@gmail.com>
2015-11-07 16:34:54 +0800

    highlight category

--- app/controllers/productListCtrl.js
+++ app/controllers/productListCtrl.js
@@ -1,5 +1,6 @@
 angular.module("sportsStore")
-  .controller("productListCtrl", function($scope, $filter) {
+  .constant("productListActiveClass", "btn-primary")
+  .controller("productListCtrl", function($scope, $filter, productListActiveClass) {
     var selectedCategory = null;
 
     $scope.selectCategory = function(newCategory) {
@@ -8,5 +9,9 @@ angular.module("sportsStore")
 
     $scope.categoryFilterFn = function(product) {
       return selectedCategory == null || product.category == selectedCategory;
+    }
+
+    $scope.getCategoryClass = function(category) {
+      return selectedCategory == category ? productListActiveClass : "";
     }
   });
--- app/controllers/sportsStore.js
+++ app/controllers/sportsStore.js
@@ -5,7 +5,7 @@ angular.module("sportsStore")
       {name: "Product #1", description: "A Product", category: "Category #1", price: 100},
       {name: "Product #2", description: "B Product", category: "Category #2", price: 110},
       {name: "Product #3", description: "C Product", category: "Category #3", price: 200},
-      {name: "Product #4", description: "D Product", category: "Category #4", price: 210}
+      {name: "Product #4", description: "D Product", category: "Category #1", price: 210}
     ]
   };
 });
