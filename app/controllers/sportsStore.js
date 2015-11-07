9d501dd419f54415becb7882ea2834f92a0f3290
Longyang Zhang <zhanglongyang.cn@gmail.com>
2015-11-07 17:36:31 +0800

    use real data from deployd

--- app/controllers/sportsStore.js
+++ app/controllers/sportsStore.js
@@ -1,11 +1,13 @@
 angular.module("sportsStore")
-.controller("sportsStoreCtrl", function($scope) {
-  $scope.data = {
-    products: [
-      {name: "Product #1", description: "A Product", category: "Category #1", price: 100},
-      {name: "Product #2", description: "B Product", category: "Category #2", price: 110},
-      {name: "Product #3", description: "C Product", category: "Category #3", price: 200},
-      {name: "Product #4", description: "D Product", category: "Category #1", price: 210}
-    ]
-  };
+.constant("dataUrl", "http://localhost:5500/products")
+.controller("sportsStoreCtrl", function($scope, $http, dataUrl) {
+  $scope.data = {};
+
+  $http.get(dataUrl)
+    .success(function(data) {
+      $scope.data.products = data;
+    })
+    .error(function(error) {
+      $scope.data.error = error;
+    });
 });
